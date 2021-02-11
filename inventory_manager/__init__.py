f5c2d5b5a99fd675a461793f335a85758efbb3ec
NagariaHussain <hussainbhaitech@gmail.com>
2021-02-11 13:57:02 +0530

    Add comments to main __init__  and db file

--- inventory_manager/__init__.py
+++ inventory_manager/__init__.py
@@ -1,7 +1,11 @@
+# Std lib imports
 import os
+
+# Flask imports
 from flask import Flask
 
 def create_app():
+    '''creates, configures and returns a flask app instances'''
     # Create flask instance
     app = Flask(__name__, instance_relative_config=True)
 
@@ -22,7 +26,7 @@ def create_app():
     from . import db
     db.init_app(app)
 
-    # Import blueprints
+    # Import blueprints libs
     from . import product, location, report, product_movement
 
     # Add blueprints
@@ -31,7 +35,8 @@ def create_app():
     app.register_blueprint(report.bp)
     app.register_blueprint(product_movement.bp)
 
-    # To make '/' equivalent to '/index'
+    # To make '/' equivalent 
+    # to 'index' (for url_for)
     app.add_url_rule('/', endpoint='index')
     
     # return this app instance
--- inventory_manager/db.py
+++ inventory_manager/db.py
@@ -1,5 +1,5 @@
+# Imports
 import sqlite3
-
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
